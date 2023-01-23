fe36c2aa31478e5cd6032086bbf730a34ee29dbf
prajwal-git-hub <prajwal.ratnaparkhe18@gmail.com>
2023-01-23 10:51:50 +0530

    login form changed

--- scrap-portal/src/Inner/Login.js
+++ scrap-portal/src/Inner/Login.js
@@ -5,18 +5,14 @@ import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-
-
-
 export const Login = () => {
-  const test = sessionStorage.getItem('Auth Token');
+  const test = sessionStorage.getItem("Auth Token");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const navigate = useNavigate();
   console.log(test);
 
- 
   const signUp = () => {
     navigate("/signup");
   };
@@ -56,7 +52,7 @@ export const Login = () => {
 
   return (
     <>
-      <div className="App-header">
+      {/* <div className="App-header">
         <ToastContainer />
         <div className="login-box">
           <h2>Login</h2>
@@ -126,6 +122,63 @@ export const Login = () => {
               </button>
             </div>
           </form>
+        </div>
+      </div> */}
+
+      <div className="container">
+        <ToastContainer />
+        <div className="card card-container">
+          <img
+            id="profile-img"
+            className="profile-img-card"
+            src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
+            alt="user-logo"
+          />
+          <p id="profile-name" className="profile-name-card"></p>
+          <form className="form-signin" onSubmit={handelsubmit}>
+            <span id="reauth-email" className="reauth-email"></span>
+            <input
+              type="email"
+              id="inputEmail"
+              className="form-control"
+              placeholder="Email address"
+              required
+              autofocus
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              id="inputPassword"
+              className="form-control"
+              placeholder="Password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          
+            <button
+              className="btn btn-lg btn-primary btn-block btn-signin"
+              type="submit"
+            >
+              Sign in
+            </button>
+          </form>
+
+
+          <button
+                type="submit"
+                onClick={signUp}
+              >
+                 Don'have account ?
+
+              </button>
+
+
+          <button type="submin" onClick={forgot} style={{marginTop:"5px"}}>
+            {" "}
+            Forgot Password ?
+          </button>
         </div>
       </div>
     </>
